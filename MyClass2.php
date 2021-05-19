8156a6daedf6d8db6352f38cf5e976910c18a245
anass-hiit <anass.n@hiitconsulting.com>
2021-05-19 15:43:22 +0100

    feat(gateway): my branch

--- MyClass2.php
+++ MyClass2.php
@@ -7,4 +7,28 @@ class MyClass
   public $toto;
   public $camel;
   public  $snack_case;
+  public $br1;
+
+    /**
+     * @return mixed
+     */
+    public function getBr1()
+    {
+        return $this->br1;
+    }
+
+    /**
+     * @param mixed $br1
+     *
+     * @return MyClass
+     */
+    public function setBr1($br1)
+    {
+        $this -> br1 = $br1;
+
+        return $this;
+    }
+
+
+
 }
