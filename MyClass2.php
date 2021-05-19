c64fcde119c895a184fe5bda9810d4dc88ea574c
anass-hiit <anass.n@hiitconsulting.com>
2021-05-19 15:47:15 +0100

    feat(gateway): add br2

--- MyClass2.php
+++ MyClass2.php
@@ -7,4 +7,26 @@ class MyClass
   public $toto;
   public $camel;
   public  $snack_case;
+  public $br2;
+
+    /**
+     * @return mixed
+     */
+    public function getBr2()
+    {
+        return $this -> br2;
+    }
+
+    /**
+     * @param mixed $br2
+     *
+     * @return MyClass
+     */
+    public function setBr2($br2)
+    {
+        $this -> br2 = $br2;
+
+        return $this;
+    }
+
 }
